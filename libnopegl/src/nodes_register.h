4d19b5072e49cfdd0b938116d8e1aa2de3359d78
Clément Bœsch <u@pkh.me>
2022-05-16 23:28:15 +0200

    nodes: add RenderHistogram
    This dedicated render essentially reads ColorStats.summary, which
    corresponds to the global color distribution of the analyzed texture.

--- libnopegl/src/nodes_register.h
+++ libnopegl/src/nodes_register.h
@@ -126,6 +126,7 @@
     action(NGL_NODE_RENDERCOLOR,            ngli_rendercolor_class)             \
     action(NGL_NODE_RENDERGRADIENT,         ngli_rendergradient_class)          \
     action(NGL_NODE_RENDERGRADIENT4,        ngli_rendergradient4_class)         \
+    action(NGL_NODE_RENDERHISTOGRAM,        ngli_renderhistogram_class)         \
     action(NGL_NODE_RENDERTEXTURE,          ngli_rendertexture_class)           \
     action(NGL_NODE_RENDERTOTEXTURE,        ngli_rtt_class)                     \
     action(NGL_NODE_RESOURCEPROPS,          ngli_resourceprops_class)           \
