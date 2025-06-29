033f7954ac1cfcd2eecb22789ced133cab87bb46
Clément Bœsch <u@pkh.me>
2025-06-29 23:30:53 +0200

    wip

--- python/pynopegl-utils/pynopegl_utils/viewer/config.py
+++ python/pynopegl-utils/pynopegl_utils/viewer/config.py
@@ -23,6 +23,7 @@
 import json
 import os
 import os.path as op
+import sys
 from pathlib import Path
 from typing import Tuple
 
@@ -34,65 +35,68 @@ class Config(QtCore.QObject):
     FILEPATH = (
         Path(os.environ.get("XDG_DATA_HOME", Path.home() / ".local" / "share")) / "nope.gl" / "viewer.json"
     ).as_posix()
-    CHOICES = {
-        "framerate": [
-            (8, 1),
-            (12, 1),
-            (15, 1),
-            (24000, 1001),
-            (24, 1),
-            (25, 1),
-            (30000, 1001),
-            (30, 1),
-            (50, 1),
-            (60000, 1001),
-            (60, 1),
-        ],
-        "export_res": list(RESOLUTIONS.keys()),
-        "export_profile": list(ENCODE_PROFILES.keys()),
-        "export_samples": [0, 2, 4, 8],
-    }
+
+    FRAMERATES = [
+        (8, 1),
+        (12, 1),
+        (15, 1),
+        (24000, 1001),
+        (24, 1),
+        (25, 1),
+        (30000, 1001),
+        (30, 1),
+        (50, 1),
+        (60000, 1001),
+        (60, 1),
+    ]
+    EXPORT_RESOLUTIONS = list(RESOLUTIONS.keys())
+    EXPORT_PROFILES = list(ENCODE_PROFILES.keys())
+    EXPORT_SAMPLES = [0, 2, 4, 8]
 
     def __init__(self):
         super().__init__()
 
-        self._cfg = {
-            # Script
-            "script": "pynopegl_utils.viewer.scenes",
-            "scene": "demo",
-            # Rendering settings
-            "framerate": (60, 1),
-            # Export
-            "export_filename": (Path.home() / "nope.mp4").as_posix(),
-            "export_res": "1080p",
-            "export_profile": "mp4_h264_420",
-            "export_samples": 0,
-        }
+        self._init_fields()
+        with open(self.FILEPATH) as f:
+            self._load_config(json.load(f))
 
-        self._needs_saving = False
-
-        if op.exists(self.FILEPATH):
-            with open(self.FILEPATH) as f:
-                self._cfg.update(self._sanitized_config(json.load(f)))
-        else:
-            self._needs_saving = True
+        self._needs_saving = not op.exists(self.FILEPATH)
 
         self._config_timer = QtCore.QTimer()
         self._config_timer.setInterval(1000)  # every second
         self._config_timer.timeout.connect(self._check_config)
         self._config_timer.start()
 
-    def _sanitized_config(self, cfg):
-        out_cfg = {}
-        for key, value in cfg.items():
-            allowed_values = self.CHOICES.get(key)
-            if isinstance(value, list):
-                value = tuple(value)
-            if allowed_values is None or value in allowed_values:
-                out_cfg[key] = value
-            else:
-                print(f"warning: {value} not allowed for {key}")
-        return out_cfg
+    def _init_fields(self):
+        # Script
+        self._script = "pynopegl_utils.viewer.scenes"
+        self._scene = "demo"
+
+        # Rendering settings
+        self._framerate = (60, 1)
+        self._export_filename = (Path.home() / "nope.mp4").as_posix()
+
+        # Export
+        self._export_res = "1080p"
+        self._export_profile = "mp4_h264_420"
+        self._export_samples = 0
+
+    def _load_config(self, data):
+        self.script = data.get("script", self.script)
+        self.scene = data.get("demo", self.scene)
+
+        framerate = data.get("framerate", self.framerate)
+        if framerate in self.FRAMERATES:
+            self.framerate = framerate
+
+        self.export_filename = data.get("export_filename", self.export_filename)
+
+        export_res = data.get("export_res", self.export_res)
+        if export_res in RESOLUTIONS.keys():
+            self.export_res = export_res
+
+        self.export_profile = data.get("export_profile", self.export_profile)
+        self.export_samples = data.get("export_samples", self.export_samples)
 
     @QtCore.Slot()
     def _check_config(self):
@@ -103,38 +107,85 @@ class Config(QtCore.QObject):
         os.makedirs(config_dir, exist_ok=True)
 
         config_file = open(self.FILEPATH, "w")
-        json.dump(self._cfg, config_file, indent=4)
+
+        cfg = dict(
+            script=self.script,
+            scene=self.scene,
+            framerate=self.framerate,
+            export_filename=self.export_filename,
+            export_res=self.export_res,
+            export_profile=self.export_profile,
+            export_samples=self.export_samples,
+        )
+        json.dump(cfg, config_file, indent=4)
         config_file.close()
 
         self._needs_saving = False
 
-    def get(self, key):
-        ret = self._cfg.get(key)
-        return tuple(ret) if isinstance(ret, list) else ret
+    @property
+    def script(self) -> str:
+        return self._script
 
-    def _set_cfg(self, key, value):
-        if self._cfg.get(key) == value:
+    @property
+    def scene(self) -> str:
+        return self._scene
+
+    @property
+    def framerate(self) -> Tuple[int, int]:
+        return self._framerate
+
+    @property
+    def export_filename(self) -> str:
+        return self._export_filename
+
+    @property
+    def export_res(self) -> str:
+        return self._export_res
+
+    @property
+    def export_profile(self) -> str:
+        return self._export_profile
+
+    @property
+    def export_samples(self) -> int:
+        return self._export_samples
+
+    @script.setter
+    def script(self, script: str):
+        self._script = script
+
+    @scene.setter
+    def scene(self, scene: str):
+        self._scene = scene
+
+    @framerate.setter
+    def framerate(self, fr: Tuple[int, int]):
+        if fr not in self.FRAMERATES:
+            print(f"warning: unsupported framerate {fr}", file=sys.stderr)
             return
-        self._cfg[key] = value
-        self._needs_saving = True
+        self._framerate = fr
 
-    def set_script(self, script: str):
-        self._set_cfg("script", script)
+    @export_filename.setter
+    def export_filename(self, filename: str):
+        self._export_filename = filename
 
-    def set_scene(self, scene: str):
-        self._set_cfg("scene", scene)
+    @export_res.setter
+    def export_res(self, res: str):
+        if res not in self.EXPORT_RESOLUTIONS:
+            print(f"warning: unsupported resolution {res}", file=sys.stderr)
+            return
+        self._export_res = res
 
-    def set_framerate(self, fr: Tuple[int, int]):
-        self._set_cfg("framerate", fr)
+    @export_profile.setter
+    def export_profile(self, profile: str):
+        if profile not in self.EXPORT_PROFILES:
+            print(f"warning: unsupported export profile {profile}", file=sys.stderr)
+            return
+        self._export_profile = profile
 
-    def set_export_filename(self, filename: str):
-        self._set_cfg("export_filename", filename)
-
-    def set_export_res(self, res: int):
-        self._set_cfg("export_res", res)
-
-    def set_export_profile(self, profile: str):
-        self._set_cfg("export_profile", profile)
-
-    def set_export_samples(self, samples: int):
-        self._set_cfg("export_samples", samples)
+    @export_samples.setter
+    def export_samples(self, samples: int):
+        if samples not in self.EXPORT_SAMPLES:
+            print(f"warning: unsupported samples {samples}", file=sys.stderr)
+            return
+        self._export_samples = samples
